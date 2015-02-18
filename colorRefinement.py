19ab021d56a97770c6ff3394ebf5493167f8d16e
Tim Kemp <t.kemp@student.utwente.nl>
2015-02-18 16:54:38 +0100

    yeey

--- colorRefinement.py
+++ colorRefinement.py
@@ -2,7 +2,6 @@ __author__ = 'Tim'
 
 from graphIO import *
 
-
 def refine(g):
 	colordict = { }
 	for v in g.V():
@@ -11,18 +10,28 @@ def refine(g):
 			colordict[v.a] = [v]
 		else:
 			colordict[v.a].append(v)
-	print(colordict)
-	print(colordict.values())
 	changed = True
+	colorrangeinc = len(g.V())
+	colorvalue = 0
 	while changed:
+		tempcolordict = colordict
 		for key in colordict.keys():
 			buren = []
-			andereburen = []
+			colorvalue += colorrangeinc
+			colorinc2 = 1
 			for value in colordict.get(key):
 				nc = getNeighbourColors(value)
-
+				if nc not in buren and len(buren) != 0:
+					buren.append(nc)
+					value.a = colorvalue + colorinc2
+					colorinc2 += 1
+				elif len(buren) == 0:
+					buren.append(nc)
+		if tempcolordict == colordict:
+			changed = False
+	for node in g.V():
+		print(node.a, colorvalue)
 	return None
-
 
 def getNeighbourColors(v):
 	colors = []
@@ -30,6 +39,5 @@ def getNeighbourColors(v):
 		colors.append(i.a)
 		colors.sort()
 	return colors
-
 
 refine(loadgraph("GI_TestInstancesWeek1/crefBM_4_7.grl", readlist=False))
