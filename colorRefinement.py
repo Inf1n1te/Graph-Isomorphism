11db1b9a39eee130f3e930ca9dfd10d7e80616a1
Tim108 <tsonderen108@gmail.com>
2015-02-18 17:20:54 +0100

    nog een ding gedaan

--- colorRefinement.py
+++ colorRefinement.py
@@ -2,8 +2,9 @@ __author__ = 'Tim'
 
 from graphIO import *
 
+
 def refine(g):
-	colordict = { }
+	colordict = {}
 	for v in g.V():
 		v.a = v.deg()
 		if not colordict.__contains__(v.a):
@@ -35,6 +36,7 @@ def refine(g):
 	print(finalcolors)
 	return finalcolors
 
+
 def getNeighbourColors(v):
 	colors = []
 	for i in v.nbs():
@@ -43,4 +45,26 @@ def getNeighbourColors(v):
 	return colors
 
 
-refine(loadgraph("GI_TestInstancesWeek1/crefBM_4_16.grl", readlist=False))
+def compare(x):
+	a = x[0]
+	b = []
+	d = []
+	r = []
+	for i in range(len(a)):
+		b.append(refine(a[i]).sort())
+
+	for j in range(len(b)):
+		if j not in d:
+			d.append(j)
+			l = []
+			for k in range(len(b)):
+				if k not in d and b[k] == b[j]:
+					l.append(b(k))
+					d.append(b(k))
+			if len(l) is not 0:
+				r.append(l)
+	return r
+
+
+compare(loadgraph("GI_TestInstancesWeek1/crefBM_4_16.grl", readlist=True))
+# refine(loadgraph("GI_TestInstancesWeek1/crefBM_4_16.grl", readlist=False))
