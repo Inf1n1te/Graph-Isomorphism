9299cfb203e1a5295fc032f9fcc9f25c499dc76d
Tim Kemp <t.kemp@student.utwente.nl>
2015-03-04 14:46:44 +0100

    preprocessing

--- colorRefinement.py
+++ colorRefinement.py
@@ -93,18 +93,43 @@ def splitlist(l, n):
 	return [l[i:i + n] for i in range(0, len(l), n)]
 
 
-def preprocessing(g):
-	modules = {}
+# Alleen False Twins werkt nog
+def preprocessing(g):  #Maakt modules van (False) Twins (improvement 2)
+	falsetwins = {}
+	twins = {}
 	for i in range(len(g.V())):
 		vertex1 = g.V()[i]
+		nbs1 = tuple(vertex1.nbs())
+		nbs1app = vertex1.nbs()
+		print(nbs1app, i)
+		nbs1app.append(vertex1)
+		nbs1ext = tuple(nbs1app)
+		#print(nbs1ext)
 		for vertex2 in g.V()[i + 1:]:
-			if vertex1.nbs() == vertex2.nbs():
-				if vertex1.nbs() in modules.keys():
-					modules[vertex1.nbs()].append(vertex1)
-					modules[vertex1.nbs()].append(vertex2)
+			nbs2 = tuple(vertex2.nbs())
+			nbs2app = vertex2.nbs()
+			nbs2app.append(vertex2)
+			nbs2ext = tuple(nbs2app)
+			# print(nbs1ext,nbs2ext)
+			if nbs1 == nbs2:  #False twins
+				if nbs1 in falsetwins.keys():
+					falsetwins[nbs1].append(vertex1)
+					falsetwins[nbs1].append(vertex2)
 				else:
-					modules[vertex1.nbs()] = [vertex1, vertex2]
-	return modules
+					falsetwins[nbs1] = [vertex1, vertex2]
+			elif nbs1ext == nbs2ext:  # Twins
+				if nbs1ext in twins.keys():
+					twins[nbs1ext].append(vertex1)
+					twins[nbs1ext].append(vertex2)
+				else:
+					twins[nbs1ext] = [vertex1, vertex2]
+	return falsetwins, twins
 
 # print(splitList([1, 2, 3, 4, 5, 6, 7, 8], 3))
-print(comparegraphs(loadgraph("GI_TestInstancesWeek1/crefBM_6_15.grl", readlist=True)))
+print(comparegraphs(loadgraph("GI_TestInstancesWeek1/crefBM_4_7.grl", readlist=True)))
+
+
+# test preprocessing
+#aa = loadgraph("GI_TestInstancesWeek1/crefBM_4_7copy.grl", readlist=False)
+#print(aa)
+#print(preprocessing(aa))
