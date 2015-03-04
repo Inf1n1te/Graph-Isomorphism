74e4c66d063e52be112673781e4b66d65e0d12a2
Inf1n1te <timkerkhoven@live.nl>
2015-03-04 13:28:20 +0100

    grote veranderingen

--- colorRefinement.py
+++ colorRefinement.py
@@ -3,10 +3,13 @@ __author__ = 'Tim108'
 from makegraphs import disjointunion
 from graphIO import *
 
+numberOfGraphs = 0
+graphlist = []
+
 
 def refine(g):
 	# initialize
-	colordict = {}  # dictionary with key=colornum and value=vertex array
+	colordict = { }  # dictionary with key=colornum and value=vertex array
 	for v in g.V():
 		v.colornum = v.deg()
 		if v.colornum not in colordict:
@@ -48,7 +51,14 @@ def refine(g):
 		finalcolors.append(node.colornum)
 
 	# DUS HIER SHIT DOEN MET COLORDICT EN DUBBELE KLEUREN ENZO
-	return colordict
+
+	# split > compareColor > recursive refine
+	done, todo = compareColors(splitColorDict(colordict, g))
+	h = disjoint(todo)
+	if h is not None:
+		return done.extend(refine(h))
+	else:
+		return done
 
 
 def getNeighbourColors(v):
@@ -58,8 +68,10 @@ def getNeighbourColors(v):
 	return colors
 
 
-def compare(x):
+def compareOLD(x):  # OLD
 	graphs = x[0][0]
+	global numberOfGraphs
+	numberOfGraphs = len(x[0])
 	for y in range(1, len(x[0])):
 		graphs = disjointunion(graphs, x[0][y])
 	coloredgraphs = refine(graphs)
@@ -73,10 +85,52 @@ def compare(x):
 			for list in partitions:
 				if int(value.__repr__()) in list:
 					split[partitions.index(list)].append(value.colornum)
+	# print(split)
 	return split
 
 
-def comparegraphs(x):
+def compare(graphlisturl):
+	global graphlist
+	graphlist = loadgraph(graphlisturl, readlist=True)
+	return refine(disjoint())
+
+
+def disjoint(graphnumbers=-1):
+	if not graphnumbers:
+		return None
+
+	graphs = graphlist[0][0]
+	global numberOfGraphs
+
+	if graphnumbers == -1:
+		for y in range(1, len(graphlist[0])):
+			numberOfGraphs = len(graphlist[0])
+			graphs = disjointunion(graphs, graphlist[0][y])
+	else:
+		numberOfGraphs = len(graphnumbers)
+		print("---", numberOfGraphs, graphnumbers)
+		graphs = graphlist[0][graphnumbers.pop(0)]
+		for y in graphnumbers:
+			graphs = disjointunion(graphs, graphlist[0][y])
+	return graphs
+
+
+def splitColorDict(colordict, g):
+	partitions = splitlist(range(len(g.V())), int(len(g.V()) / numberOfGraphs))
+	print(numberOfGraphs, len(partitions))
+	split = []
+	for j in range(numberOfGraphs):
+		split.append([])
+	for key in colordict:
+		for value in colordict.get(key):
+			for e in partitions:
+				if int(value.__repr__()) in e:
+					split[partitions.index(e)].append(value.colornum)
+	# print(split)
+	return split
+
+
+def comparegraphs(x):  # OLD
 	r = []
 	ccs = compare(x)
 	undecided = []
@@ -88,11 +142,26 @@ def comparegraphs(x):
 			if i != j and i < j:
 				if ccs[i] == ccs[j] and i not in undecided:
 					r.append([i, j])
+	return r, "-----", undecided
+
+
+def compareColors(split):
+	r = []
+	undecided = []
+	for i in range(len(split)):
+		if len(split[i]) > len(set(split[i])):
+			undecided.append(i)
+	for i in range(len(split)):
+		for j in range(len(split)):
+			if i != j and i < j:
+				if split[i] == split[j] and i not in undecided:
+					r.append([i, j])
 	return r, undecided
+
 
 def splitlist(l, n):
 	return [l[i:i + n] for i in range(0, len(l), n)]
 
 
 # print(splitList([1, 2, 3, 4, 5, 6, 7, 8], 3))
-print(comparegraphs(loadgraph("GI_TestInstancesWeek1/crefBM_6_15.grl", readlist=True)))
+print(compare("GI_TestInstancesWeek1/crefBM_4_7.grl"))
