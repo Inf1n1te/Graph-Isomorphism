b9cdf89459ecdbdaf59659f712a784f8a0b573c7
Tim Kemp <t.kemp@student.utwente.nl>
2015-03-18 17:23:45 +0100

    preprocessing

--- colorRefinement.py
+++ colorRefinement.py
@@ -1,9 +1,11 @@
 __author__ = 'Tim (& [Jeroen])'
 
 import time
+import collections
 
 from makegraphs import disjointunion
 from graphIO import *
+
 
 
 numberOfGraphs = 0
@@ -134,45 +136,71 @@ def compareColors(split):
 def splitlist(l, n):
 	return [l[i:i + n] for i in range(0, len(l), n)]
 
-
-# Alleen False Twins werkt nog
 def preprocessing(g):  # Maakt modules van (False) Twins (improvement 2)
 	falsetwins = {}
 	twins = {}
 	for i in range(len(g.V())):
 		vertex1 = g.V()[i]
+		# nbs1 = tuple(vertex1.nbs())
 		nbs1 = vertex1.nbs()
-		# nbs1ext = nbs1[:]
-		#nbs1 = tuple(nbs1)
-		#nxt = next((x for x in nbs1ext if x._label > vertex1._label), None)
-		#print(nxt, type(nxt))
-		#nbs1ext.insert(nxt-1, vertex1)
-		#nbs1ext = tuple(nbs1ext)
+		nbs1ext = nbs1[:]
+		nbs1ext.append(vertex1)
+		nbs2 = tuple(nbs1)
+		nbs1ext.sort(key=lambda x: x._label)
+		nbs1ext = tuple(nbs1ext)
 		for vertex2 in g.V()[i + 1:]:
+			#nbs2 = tuple(vertex2.nbs())
 			nbs2 = vertex2.nbs()
-			# nbs2ext = nbs2[:]
-			#nbs2ext.append(vertex2)
-			#nbs2 = tuple(nbs2)
-			#nbs2ext.sort(key=lambda x: x._label)
-			#nbs2ext = tuple(nbs2ext)
-			# print(nbs1,nbs2,nbs1ext, nbs2ext)
+			nbs2ext = nbs2[:]
+			nbs2ext.append(vertex2)
+			nbs2 = tuple(nbs2)
+			nbs2ext.sort(key=lambda x: x._label)
+			nbs2ext = tuple(nbs2ext)
+			#print(nbs1,nbs2,nbs1ext, nbs2ext)
 			if nbs1 == nbs2:  #False twins
 				if nbs1 in falsetwins.keys():
 					falsetwins[nbs1].append(vertex1)
 					falsetwins[nbs1].append(vertex2)
 				else:
 					falsetwins[nbs1] = [vertex1, vertex2]
-				# elif nbs1ext == nbs2ext:  # Twins
-				#	if nbs1ext not in twins.keys():
-				#		twins[nbs1ext] = nbs1ext
-	return list(falsetwins.keys())  #, list(twins.keys())
+			elif nbs1ext == nbs2ext:  # Twins
+				if nbs1ext not in twins.keys():
+					twins[nbs1ext] = nbs1ext
+	return list(falsetwins.values()), list(twins.values())
+
+
+def preprocessingfast(g):  # Maakt modules van (False) Twins (improvement 2)
+	falsetwins = {}
+	for i in range(len(g.V())):
+		vertex1 = g.V()[i]
+		nbs1 = tuple(vertex1.nbs())
+		for vertex2 in g.V()[i + 1:]:
+			nbs2 = tuple(vertex2.nbs())
+			if nbs1 == nbs2:  # False twins
+				if nbs1 in falsetwins.keys():
+					falsetwins[nbs1].append(vertex1)
+					falsetwins[nbs1].append(vertex2)
+				else:
+					falsetwins[nbs1] = [vertex1, vertex2]
+	return list(falsetwins.values())
+
+
+def preprocessingfaster(g):  # Tweede deel O(n)
+	nbs = []
+	for vertex in g.V():
+		nbs.append(tuple(vertex.nbs()))
+	falsetwins = collections.defaultdict(list)
+	for i, item in enumerate(nbs):
+		falsetwins[item].append(i)
+	falsetwins = {k: v for k, v in falsetwins.items() if len(v) > 1}
+	return falsetwins
 
 
 # test preprocessing
 print('start while')
 start_time = time.clock()
 aa = loadgraph("GI_TestInstancesWeek1/crefBM_4_4098.grl", readlist=False)
-print(preprocessing(aa))
+print(preprocessingfaster(aa))
 elapsed_time = time.clock() - start_time
 print('a: {0:.4f} sec'.format(elapsed_time))
 
